e7d4f2e7511a4ae904f397f60e68223eab03df56
Ignacio Font <ignacio.font@mercadolibre.com>
2019-12-09 17:05:53 -0300

    add stuff

--- cli.js
+++ cli.js
@@ -11,11 +11,16 @@ const readInterface = readline.createInterface({
 });
 
 const writeFile = (repoName) => {
-    // const repoName = cloneUrl.match(/.+?(?=.git)/g);
-    // fs.createReadStream('PULL_REQUEST_TEMPLATE.md').pipe(fs.createWriteStream('holaaaa.md'));
     fs.copyFile(`../PULL_REQUEST_TEMPLATE.md`, `./${repoName}/PULL_REQUEST_TEMPLATE.md`, (err) => {
         if (err) throw err;
         console.log('el copiado salió bien');
+        shell.cd(repoName)
+        shell.exec(`git checkout -b hotfix/updatePrTemplateScript`);
+        shell.exec(`git add .`);
+        shell.exec(`git commit -m 'update pr template'`);
+        shell.exec(`git push origin hotfix/updatePrTemplateScript`);
+        shell.cd('..')
+
     });
 };
 
@@ -24,6 +29,5 @@ readInterface.on('line', function(cloneUrl) {
     console.log({ repoName })
     shell.cd(resolvedPath)
     shell.exec(`git clone ${cloneUrl}`, () => writeFile(repoName));
-
     console.log(cloneUrl)
 });
